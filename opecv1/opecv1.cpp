1ecb73ea8211fb8db9890429ab2c1f6317af431d
Piotr Morzydusza <morzydusza@gmail.com>
2016-03-29 22:28:15 +0200

    pseudo code added, refactoring

--- opecv1/opecv1.cpp
+++ opecv1/opecv1.cpp
@@ -11,15 +11,16 @@ using namespace std;
 int ox = 305; 
 int oy = 248; 
  
-void CallBackFunc(int event, int x, int y, int flags, void* userdata)
-{
-	if (flags == (EVENT_FLAG_CTRLKEY + EVENT_FLAG_LBUTTON))
-	{
-		cout << "Left mouse button is clicked while pressing CTRL key - position (" << x << ", " << y << ")" << endl;
-		ox = x; 
-		oy = y; 
-	}
-}
+
+//void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+//{
+//	if (flags == (EVENT_FLAG_CTRLKEY + EVENT_FLAG_LBUTTON))
+//	{
+//		cout << "Left mouse button is clicked while pressing CTRL key - position (" << x << ", " << y << ")" << endl;
+//		ox = x; 
+//		oy = y; 
+//	}
+//}
 
 
 int main(int argc, char* argv[])
@@ -35,13 +36,11 @@ int main(int argc, char* argv[])
 
 	double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
 
-	cout << "Frame per seconds : " << fps << endl;
-
 	namedWindow("MyVideo", CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
 
-	setMouseCallback("MyVideo", CallBackFunc, NULL);
+	//setMouseCallback("MyVideo", CallBackFunc, NULL);
 
-	vector<Vec3b> res;
+	vector<Mat> frames;
 
 	while (1)
 	{
@@ -52,9 +51,10 @@ int main(int argc, char* argv[])
 			cout << "Cannot read the frame from video file" << endl;
 			break;
 		}
-		Vec3b color = frame.at<Vec3b>(ox, oy);
-		res.push_back(color); 
-		cout << color; 
+
+		//Vec3b color = frame.at<Vec3b>(ox, oy);
+		frames.push_back(frame); 
+
 		imshow("MyVideo", frame); //show the frame in "MyVideo" window
 
 		if (waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
@@ -62,6 +62,14 @@ int main(int argc, char* argv[])
 			cout << "esc key is pressed by user" << endl;
 			break;
 		}
+		cout << frames.size();
+
+		// klatki to frames
+		// max_t - d³ugoœæ cyklu to ok 3500 ms  czyli ok 105 klatek
+		// Ei - empirycznie ale jak dla mnie trzeba daæ co najmniej tysi¹c, jeszcze zale¿y czy bêdziemy to 
+		// rzucaæ do szaroœci i wycinaæ pod³ogê czy nie . 
+		// tu mo¿na u¿ywaæ Analyzer.cpp wiêc tam zaimplementujcie ten pseudokod 
+
 	}
 	
 	return 0;
